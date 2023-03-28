9bf6880ff61ff2ee4db1482327e676397a483bfd
Ren Blake <rcblake06@gmail.com>
2023-03-28 12:36:23 -0700

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+
+
+
+function writeCards(people, celebraion) {
+    let allCards = []
+    for (let i = 0; i < people.length; i++) {
+        allCards.push(`Thank you, ${people[i]}, for the wonderful ${celebraion} gift!`);
+       
+    }
+    return allCards
+}
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise")
+
+let i = 10
+
+function countDown(i) {
+    while (i > -1) {
+        console.log(i);
+        i--
+    }
+}
+
+countDown(i)
